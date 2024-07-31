b5d237b2ea758b77f29aaddb97ab3fc429f5be81
ArunKumar1977 <AK7208@CORP.CYIENT.COM>
2024-07-31 22:21:56 +0530

    terraform resource group

--- provider.tf
+++ provider.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 terraform {
   required_providers {
     azurerm = {
@@ -13,19 +12,3 @@ provider "azurerm" {
   skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
 }
-=======
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
-}
-
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
-  features {}
-}
->>>>>>> 3ef5d0a6af484fbfc9f5930cb6d93c03d8232763
