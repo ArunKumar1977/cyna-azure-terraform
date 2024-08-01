6d31f1033ff7752120976bff840419ba77f4451c
ArunKumar1977 <AK7208@CORP.CYIENT.COM>
2024-08-01 18:09:11 +0530

    added 2 subnets

--- a4.subnet.tf
+++ a4.subnet.tf
@@ -1,7 +1,7 @@
 resource "azurerm_subnet" "websubnet" {
   name = var.web_subnet_name
   resource_group_name = azurerm_resource_group.rg.name #once the subnet get created it should be part of your resource group
-  virtual_network_name = azurerm_virtual_network.vpc.name #subnet should be part of your vnet also
+  virtual_network_name = azurerm_virtual_network.example.name #subnet should be part of your vnet also
   address_prefixes = var.web_subnet_address
   
 }
