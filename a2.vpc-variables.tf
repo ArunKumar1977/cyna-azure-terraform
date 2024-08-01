73127dc2b52608210848d6373586cda2efbbdce9
ArunKumar1977 <AK7208@CORP.CYIENT.COM>
2024-08-01 17:41:32 +0530

    vpc

---
+++ a2.vpc-variables.tf
@@ -0,0 +1,29 @@
+variable "vnet_name" {
+description = "virtual network name"
+type = string
+default = "vnet-default"
+}
+
+variable "vnet_address_space" {
+description = "vnet address space"
+type = list(string)
+default = ["10.0.0.0/16"]
+}
+
+variable "web_subnet_name" {
+type = string
+default = "websubnet"
+}
+variable "web_subnet_address" {
+type = list(string)
+default = ["10.0.1.0/16"]
+}
+
+variable "db_subnet_name" {
+type = string
+default = "dbsubnet"
+}
+variable "db_subnet_address" {
+type = list(string)
+default = ["10.0.2.0/16"]
+}
